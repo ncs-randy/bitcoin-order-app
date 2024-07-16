67ae30eb4d0845361913d43b0d2246e22aae1c25
ncs-randy <randy.wang@ncs.com.sg>
2024-07-16 16:01:43 +0800

    update owasp again

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull owasp/zap2docker-stable:latest
-docker run -i owasp/zap2docker-stable:latest zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+docker run -i owasp/zap2docker-stable:latest zap-baseline.py -t "https://github.com/ncs-randy/bitcoin-order-app" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
